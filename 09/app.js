cffe853a4cac8aca7438c767d22b1d49d34a99df
Mateus Camargo <camargomateus711@gmail.com>
2022-12-07 22:19:14 -0300

    009

---
+++ 09/app.js
@@ -0,0 +1,12 @@
+function verdadeirofalso() {
+    let numero = document.getElementById("numero").value
+    let falso = valor1
+    let verdadeiro = valor2
+    if
+        (valor1 == falso && valor1 == 1) {
+        resposta.innerHTML = "valor 1 é falso"
+    } else {
+        (valor2 == verdadeiro && valor2 == 1)
+        resposta.innerHTML = "valor 2 é verdadeiro"
+    }
+}
